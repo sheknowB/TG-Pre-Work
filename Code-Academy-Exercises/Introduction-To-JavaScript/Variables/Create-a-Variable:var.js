3d77a977e37525861f71ca1c3d4b9438daf88b8b
sheknowB <bailey.brandon.k@gmail.com>
2018-10-23 21:31:09 -1000

    Declared the variable 'favoriteFood', line 1 Declared the variable 'numOfSlices', line 3

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/Create-a-Variable:var.js
@@ -0,0 +1,10 @@
+var favoriteFood = 'pizza'; //Declare variable favoriteFood
+
+var numOfSlices = 8;
+console.log(favoriteFood); //Prints pizza
+console.log(numOfSlices); //Prints 8
+
+/* Declaring a variable using the 'var' keyword. General rules for naming variables:
+  1. Variable names cannot start with numbers.
+  2. Variable names are case sensitive.
+  3. Variable names cannot be the same as keywords.
