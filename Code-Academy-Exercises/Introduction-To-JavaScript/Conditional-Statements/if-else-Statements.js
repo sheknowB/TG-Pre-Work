cf4c125466f08e5ff21a6a363ab8e26111b07e94
sheknowB <bailey.brandon.k@gmail.com>
2018-10-24 09:49:24 -1000

    Introduction to if...else statements.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/if-else-Statements.js
@@ -0,0 +1,13 @@
+let sale = true;
+
+sale = false;
+
+if(sale) {
+  console.log('Time to buy!');
+} else {
+  console.log('Time to wait for a sale.')
+}
+//If sale is true, prints "Time to buy!". If sale is false, prints "Time to wait for a sale."
+
+/*If we want a code to run when our condition is false we include an else statement. An else statement must be paired with an if statement. Together they are referred to as an if...else statement.
+  If...else statements allow us to automate solutions to a yes-or-no questions known as binary decisions. */
