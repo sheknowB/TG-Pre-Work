8a63bd2133f2b26d9e259a4c080dbdcfe444a4dc
sheknowB <bailey.brandon.k@gmail.com>
2018-10-23 19:42:34 -1000

    Utilizing the length property

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Properties.js
@@ -0,0 +1,6 @@
+console.log('Teaching the world how to code'.length);
+
+/* When we introduce a new piece of data into a JavaScript program, the browser saves it as an instance of the data type.
+  These pieces of data comes with properties. You can retrieve property information by appending a period followed by the property name.
+  The . acts as an operator called a dot operator. In this exercise we are retrieving the length of a string.
+*/   
