543cdc0e09f80b2d1e194acdddcbfda12eecb5e8
sheknowB <bailey.brandon.k@gmail.com>
2018-10-23 19:20:42 -1000

    Testing out the arithmetic operators installed in Javascript.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Arithmetic-Operators.js
@@ -0,0 +1,8 @@
+console.log(36 + 3.5); //Age I'll be when we start sending people to live on Mars
+console.log(2018 - 1969); //Years it's been since we landed on the moon
+console.log(65 / 240);
+console.log(0.2708 * 100); //The percentage of the sun that is made up of helium
+
+/* In this exercise we explore arithmetic operators that are built into JavaScript.
+  + adds, - subtracts, / divides, * multiplies, and % divides and outputs the remainder.*
+*/    
