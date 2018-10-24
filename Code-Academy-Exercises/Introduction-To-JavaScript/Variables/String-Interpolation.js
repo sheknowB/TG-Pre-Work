b7ccd3158548b230ebbd95f5bbc7c5fde3a26a05
sheknowB <bailey.brandon.k@gmail.com>
2018-10-23 22:21:33 -1000

    String Interpolation. Learned the template literal.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/String-Interpolation.js
@@ -0,0 +1,6 @@
+var myName = 'Brandon'; //Declared variable named myName.
+var myCity = 'Honolulu'; //Declared variable named myCity
+console.log(`My name is ${myName}. My favorite city is ${myCity}`); //Output: My name is Brandon. My favorite city is Honolulu.
+
+/* We can insert variables into strings using template literals. A template literal is wrapped in backticks (`).
+  Placeholder looks like this ${variable}. */
