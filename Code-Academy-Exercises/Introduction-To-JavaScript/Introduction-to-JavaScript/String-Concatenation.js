f175959c9b73a56473dfbc2c6ca5fe7da9da6fcb
sheknowB <bailey.brandon.k@gmail.com>
2018-10-23 19:28:26 -1000

    Using string concatenation

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/String-Concatenation.js
@@ -0,0 +1,4 @@
+console.log('Hello ' + 'World');
+
+/* String concatenation appends strings together. We use the + operator to attach strings together.
+  The output of the concatenation will appear exactly as the string is written. */
