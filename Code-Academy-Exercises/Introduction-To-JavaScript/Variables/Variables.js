85a6aa720d16811141bfbd5e3fda816d6d7b58ac
sheknowB <bailey.brandon.k@gmail.com>
2018-10-23 21:18:14 -1000

    What are variables in JavaScript.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/Variables.js
@@ -0,0 +1,10 @@
+/* Variables is a container for a value. They are containers of information living in a computer's memory.
+  Such information as a username, account number, or personalized greeting. Variables can also label data with a descriptive name.
+  In short variables label and and store data in memory. */
+
+/* There are only a few things you can do with variables.
+  1. Create a variable with a descriptive name
+  2. Store or update information stored in a variable
+  3. Reference ("get") information stored in a variable */
+
+// It is important to understand that variables are not values. They contain values and represent them with a name.
