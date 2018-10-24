29c2dfb341f381c2c8e89459c314d88e26cbb6a4
sheknowB <bailey.brandon.k@gmail.com>
2018-10-23 20:27:06 -1000

    Use the .toUpperCase method to capitalize 'Codecademy', line 1 Use the .trim method to remove the whitespace around ' Remove Whitespace ', line 5

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Methods.js
@@ -0,0 +1,10 @@
+// Use .toUpperCase() to log 'Codecademy' in all uppercase letters
+console.log('Codecademy'.toUpperCase());
+
+// Use a string method to log the following string without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim());
+
+/* Methods are actions that we can perform. We learn a few string methods we can use.
+  In this exercise we call the toUpperCase method to convert the string 'Codecademy' to all Upper Case letters.
+  We also user the trim method to remove the spaces around the string '   Remove whitespace   '.
+*/  
