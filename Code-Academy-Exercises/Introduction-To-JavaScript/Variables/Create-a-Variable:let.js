a21a38b9741bfd0373abd9bb9ec9970d41113092
sheknowB <bailey.brandon.k@gmail.com>
2018-10-23 21:44:27 -1000

    Using the let keyword to declare a variable.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/Create-a-Variable:let.js
@@ -0,0 +1,6 @@
+let changeMe = true; //Declare variable changeMe and initialize it to true
+changeMe = false; //Reassign variable to false
+console.log(changeMe); //Output: false
+
+/* The let keyword signals that the variable can be changed. Here we declare the changeMe variable to have a value of true and later change it to false.
+  We can also declare a variable with no value. When we do this the variable will be initialized with undefined as its value. */
