9a212ceeee9c54b45180b690969ed83705b9e878
sheknowB <bailey.brandon.k@gmail.com>
2018-10-23 22:05:42 -1000

    Using the increment and decrement assigned operators.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/The-Increment-and-Decrement-Operator.js
@@ -0,0 +1,8 @@
+let gainedDollar = 3;
+let lostDollar = 50;
+gainedDollar++;
+lostDollar--;
+
+/* There is another set of mathematical assignment operators, increment and decrement.
+  The increment operator (++) increases the value of the variable by 1
+  The decrement operator (--) decreases the value of the variable by 1
