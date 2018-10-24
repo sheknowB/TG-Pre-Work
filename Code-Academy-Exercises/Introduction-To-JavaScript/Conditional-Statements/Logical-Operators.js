f05e32f85cb67d0da9f77b2d6514645eaf16c2c6
sheknowB <bailey.brandon.k@gmail.com>
2018-10-24 10:17:02 -1000

    If sleepy and tirednessLevel is greater than 8, it's time to sleep, otherwise not bed time yet.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/Logical-Operators.js
@@ -0,0 +1,19 @@
+let mood = 'sleepy';
+let tirednessLevel = 6;
+
+if (mood === 'sleepy' && tirednessLevel > 8) {
+  console.log ('time to sleep')
+} else {
+  console.log ('not bed time yet')
+}
+/* There are operators that work with boolean values called logical operators.
+  There are three logical operators:
+
+    1. The and operator (&&)
+    2. The or operator (||)
+    3. The not operator (!) also known as the bang operator
+
+  When we use the && operator both conditions must evaluate to true for the entire condition to evaluate true.
+  When we use the || operator just one of the conditions must evaluate to true for the entire condition to evalute true.
+  The ! operator negates the value of a boolean.
+*/
