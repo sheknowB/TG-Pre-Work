d65315a32a7363102d8be037ff7934b5a485667e
sheknowB <bailey.brandon.k@gmail.com>
2018-10-24 09:39:11 -1000

    If there is a sale, it's time to buy!

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/The-if-keyword.js
@@ -0,0 +1,9 @@
+let sale = true;
+sale = false;
+if (sale) {
+  console.log('Time to buy!')
+} // Prints "Time to buy!"
+
+/* We learn the structure of an if statement. It is comprised of the if keyword, followed by a set of parantheses, followed by a code block.
+  A condition would be set in the parantheses, and the code block would contain a statement or a task to be performed. */
+  
