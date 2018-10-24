d49b1fabd3df16d5d01a02b2fed51883acc438a0
sheknowB <bailey.brandon.k@gmail.com>
2018-10-23 22:13:04 -1000

    String concatenation with variables

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/String-Concatenation-with-Variables.js
@@ -0,0 +1,4 @@
+var favoriteAnimal = 'Eagle';
+console.log('My favorie animal: ' + favoriteAnimal + '.'); //Output: My favorite animal: Eagle
+
+// Like in a previous exercise, we can use the + mathematical operation to join two or more strings together even if one of those string values is being stored in a variable.
