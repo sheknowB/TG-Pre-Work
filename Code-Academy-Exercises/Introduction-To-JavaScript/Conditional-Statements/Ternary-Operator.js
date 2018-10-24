312e6e181bf5ea06dd390508af522be313eb982c
sheknowB <bailey.brandon.k@gmail.com>
2018-10-24 11:25:38 -1000

    Execute three ternary operators. Line 3, 7 and 11.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/Ternary-Operator.js
@@ -0,0 +1,15 @@
+let isLocked = false;
+
+isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
+
+let isCorrect = true;
+
+isCorrect ? console.log('Correct!') : console.log('Incorrect!');
+
+let favoritePhrase = 'Love That!';
+
+favoritePhrase === 'Love That!' ? console.log('I love that!') : console.log("I don't love that!");
+
+/* Another shorthand syntax where we use a ternary operator to replace an if...else statement.
+  Ternary operator simplifies the if...else statement. The condition comes first, followed by a (?). It the value is true the first expression executes, a (:) separates the true expression from the false expression, and if false the second expression executes.
+*/
