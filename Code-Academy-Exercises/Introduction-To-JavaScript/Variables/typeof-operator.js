8d574859473d55dd960b5a4afd8e9ef815db0e9c
sheknowB <bailey.brandon.k@gmail.com>
2018-10-23 22:25:56 -1000

    Using the typeof operator, lines 2 and 4.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/typeof-operator.js
@@ -0,0 +1,6 @@
+let newVariable = 'Playing around with typeof.';
+console.log(typeof newVariable); //Output: string
+newVariable = 1; //Reassigned newVariable with the number 1
+console.log(typeof newVariable); //Output: number
+
+// When checking the data type of a variable's value we use the typeof operator.
