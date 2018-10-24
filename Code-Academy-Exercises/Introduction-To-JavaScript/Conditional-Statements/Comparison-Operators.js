da4bfc8a2f884de4c4a4df6984c04568f537a37d
sheknowB <bailey.brandon.k@gmail.com>
2018-10-24 10:02:40 -1000

    If the hunger level goes above 7, it's time to eat, or else we can wait.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/Comparison-Operators.js
@@ -0,0 +1,18 @@
+let hungerLevel = 7;
+
+if (hungerLevel > 7) {
+  console.log ('Time to eat!')
+} else {
+  console.log ('We can eat later!')
+}
+
+/*Comparison operators compare two values and outputs a boolean value. It is best to think of them as a question.
+  We use comparison operators to evaluate these values. Comparison operators compare the value on the left to the value on the right and then outputs a true or false.
+  Some comparison operators are:
+    < - Less than
+    > - Greater than
+    <= - Less than or equal to
+    >= - Greater or equal to
+    === - Is Equal to
+    !== - Is Not equal to
+*/
