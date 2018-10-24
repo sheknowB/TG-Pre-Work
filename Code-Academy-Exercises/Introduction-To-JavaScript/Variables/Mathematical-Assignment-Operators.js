94e3ca8c712d0bdbe1129eaf50d86052806f8b25
sheknowB <bailey.brandon.k@gmail.com>
2018-10-23 22:02:05 -1000

    Trying out mathematical assignment operators

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/Mathematical-Assignment-Operators.js
@@ -0,0 +1,21 @@
+let levelUp = 10;
+let powerLevel = 9001;
+let multiplyMe = 32;
+let quarterMe = 1152;
+
+// Use the mathematical assignments in the space below:
+levelUp += 5;
+powerLevel -= 100;
+multiplyMe *= 11;
+quarterMe /= 4;
+
+// These console.log() statements below will help you check the values of the variables.
+// You do not need to edit these statements.
+console.log('The value of levelUp:', levelUp);
+console.log('The value of powerLevel:', powerLevel);
+console.log('The value of multiplyMe:', multiplyMe);
+console.log('The value of quarterMe:', quarterMe);
+
+/* We learn that you can combine variables with mathematical operations to calculate new values and assign them to a variable.
+  There is also a shorthand way of applying the mathematical operation by using mathematical assignment operators.
+  For instance if we wanted to add two values we would use the += operator, or if we wanted to multiply we would use the *= operator. */
