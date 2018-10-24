dc2edfcb94f57474300f1afc99aa707d5d880d3b
sheknowB <bailey.brandon.k@gmail.com>
2018-10-23 18:33:03 -1000

    logged my age, 36, into the console, line 1. logged the number of weeks coding, 2, into the console, line 2.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Console.js
@@ -0,0 +1,6 @@
+console.log(36);
+console.log(2);
+/* In this exercise we learn about the console. The console is a panel that displays important messages for developers. */
+/* In Javascript, the console keyword refers to an object, a collection of data and actions, that we can use in our code. */
+/* Keywords are words that are built into the Javascript language. One action that is built into the console object is the .log() method */
+/* When we write the console.log() what resides in the () gets logged to the console. */
