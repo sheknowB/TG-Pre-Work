67617f2523ac50f781eef29187c4a7e7ccad78b9
sheknowB <bailey.brandon.k@gmail.com>
2018-10-23 20:42:43 -1000

    On lines 1 and 2 we use a few Math objects (.floor, .random, and .ceil) On line 3 we use a Number object (.isInteger)

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Built-in-Objects.js
@@ -0,0 +1,8 @@
+console.log(Math.floor(Math.random() * 100)); // Prints an integer between 1 to 100
+console.log(Math.ceil(43.8)); //Prints the smallest integer greater than or equal to 43.8
+console.log(Number.isInteger(2017)); //Prints a true or false on whether 2017 is an integer
+
+/* JavaScript is full of built-in objects. With objects come methods that can perform a number of tasks.
+  In this exercise we use the Math object that performs other complex tasks besides arithmetic operations.
+  We also use the Number object.
+*/
