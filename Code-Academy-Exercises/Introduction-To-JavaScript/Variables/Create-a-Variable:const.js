da2a857f10338deec7b66c46ce5d80a9199c42b6
sheknowB <bailey.brandon.k@gmail.com>
2018-10-23 21:52:50 -1000

    Learning to declare a variable with the const keyword.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/Create-a-Variable:const.js
@@ -0,0 +1,7 @@
+const entree = "Enchiladas"; //Declare variable entree using const
+console.log(entree); //Output: Enchiladas
+entree = 'Tacos'; //This will error
+const appetizer; //This will also error
+
+/* As the name suggests, const stands for constant. A constant variable cannot be reassigned.
+  A constant variable must also have a value. If no value is present you will receive a syntax error. */
