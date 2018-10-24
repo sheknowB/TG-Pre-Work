a9fed3056d547ed8a4b080de9c82f16f5f7c390b
sheknowB <bailey.brandon.k@gmail.com>
2018-10-23 18:59:03 -1000

    Logged four data points. Two strings, two numbers. Lines 1 - 4.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Data-Types.js
@@ -0,0 +1,14 @@
+console.log('JavaScript');
+console.log(2011);
+console.log('Woohoo! I love to code! #codecademy');
+console.log(20.49);
+
+/* In this exercise we explore the different data types in Javascript. There are several fundamental types.
+  1. Numerical: Any numbers including decimals
+  2. String: Any grouping of characters enclosed with either single ('') or double ("") quotes
+  3. Boolean: True or False
+  4. Null: Absence of value
+  5. Undefined: also an absence of value but used differently from Null
+  6. Symbols: symbols are unique identifiers
+  7. Objects: Collections of related data
+*/
