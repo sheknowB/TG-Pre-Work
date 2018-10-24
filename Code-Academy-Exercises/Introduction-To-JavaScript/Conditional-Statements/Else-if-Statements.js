0ed6b380f28d06e1c0d9515a83fc47a224d73f1f
sheknowB <bailey.brandon.k@gmail.com>
2018-10-24 11:39:21 -1000

    Incorporated 3 else if statements to our original if...else statement.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/Else-if-Statements.js
@@ -0,0 +1,21 @@
+let season = 'summer';
+
+if (season === 'spring') {
+  console.log('It\'s spring! The trees are budding!');
+} else if (season === 'winter')
+{
+  console.log('It\'s winter! Everything is covered in snow.');
+} else if (season === 'fall')
+{
+  console.log('It\'s fall! Leaves are falling!');
+} else if (season === 'summer')
+{
+  console.log('It\'s sunny and warm because it\'s summer!')
+}else {
+  console.log('Invalid season.');
+}
+
+/* If you want to add more conditions to an if...else statement, you include an else if statement(s).
+  The else if statements allow for more than two possible outcomes, and you can add as many as you like.
+  else if statements come after the if and before the else in an if...else statement.
+*/
