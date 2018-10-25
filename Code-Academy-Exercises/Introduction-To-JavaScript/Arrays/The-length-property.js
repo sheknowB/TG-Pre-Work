3c917ed648fe9fb03b96c794118e1729052b6c7b
sheknowB <bailey.brandon.k@gmail.com>
2018-10-25 10:59:36 -1000

    Exploring the length property of an array.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/The-length-property.js
@@ -0,0 +1,5 @@
+const objectives = ['Learn a new languages', 'Read 52 books', 'Run a marathon'];
+
+console.log(objectives.length);
+
+//Length is a property built-in to the array. It can be accessed with .length.
