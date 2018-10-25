16570c1ccf060ad4fe772b52a4d45892381d1d62
sheknowB <bailey.brandon.k@gmail.com>
2018-10-25 11:04:32 -1000

    Implementing the push method to the chores array.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/The-push-method.js
@@ -0,0 +1,6 @@
+const chores = ['wash dishes', 'do laundry', 'take out trash'];
+
+chores.push('vacuum room','scrub toilet');//Adds two new items to chores array
+console.log(chores);
+
+/*The .push() method allows us to add items to the end of an array.
