830921dda8f31cf37fd9ca06c16c1a86632fe061
sheknowB <bailey.brandon.k@gmail.com>
2018-10-25 12:23:36 -1000

    Looping through an array

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Looping-through-Arrays.js
@@ -0,0 +1,7 @@
+const vacationSpots = ['Bali', 'Paris', 'Tulum'];
+
+// Write your code below
+for (let i = 0; i < vacationSpots.length; i++){
+  console.log(vacationSpots[i]);
+};
+//for loops can be used to cycle through an array. Here we use the array length method to set the stopping condition.
