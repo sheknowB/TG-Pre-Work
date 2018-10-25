3387c57c39507199592d41751718829a4fdf3f89
sheknowB <bailey.brandon.k@gmail.com>
2018-10-25 12:14:19 -1000

    Building a for loop

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/The-For-Loop.js
@@ -0,0 +1,10 @@
+// Write your code below
+for (let counter = 5; counter < 11; counter++ ) {
+  console.log(counter);
+};
+
+/* In a typical loop, there is an iterator variable which appears in all three expressions of a loop. The iterator variable is initialized, checked against the stopping condition, and assigned a new value on each loop iteration.
+A for loop contains three expressions:
+  1. Initialization. starts the loop and can be used to declare the iteration variable.
+  2. Stopping Condition. The condition that the iterator variable is checked against. If the condition value is true the code keeps running, false the code stops.
+  3. Iteration Statement. Used to update the iterator variable on each loop.
