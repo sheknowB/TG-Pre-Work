ab0ca9f413d030649ead57349c7d510fb85c9fbe
sheknowB <bailey.brandon.k@gmail.com>
2018-10-24 18:22:44 -1000

    Calling a function in your code.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Calling-a-Function.js
@@ -0,0 +1,12 @@
+function sayThanks() {
+  console.log('Thank you for your purchase! We appreciate your business.')
+}
+
+sayThanks();
+sayThanks();
+sayThanks();
+
+/* A function declaration only creates a function. We need to call the function in order for it to execute.
+  To call a function in your code you must type the function name followed by parantheses ().
+  We may call the function as many times as we wish.
+*/
