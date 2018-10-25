6e0495fd3464a3d2bf7ad98753cbe63db12a19a8
sheknowB <bailey.brandon.k@gmail.com>
2018-10-25 12:16:48 -1000

    Looping in reverse

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Looping-in-Reverse.js
@@ -0,0 +1,5 @@
+// The loop below loops from 0 to 3. Edit it to loop backwards from 3 to 0
+for (let counter = 3; counter >= 0; counter--){
+  console.log(counter)
+};
+//You can loop in reverse by making a few changes to the intial condition, stopping condition and the iteration variable.
