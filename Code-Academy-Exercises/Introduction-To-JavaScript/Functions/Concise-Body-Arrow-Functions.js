ce5a21e3596de14a88ea138ec74dde1c999fde3b
sheknowB <bailey.brandon.k@gmail.com>
2018-10-24 21:12:01 -1000

    Concise body

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Concise-Body-Arrow-Functions.js
@@ -0,0 +1,3 @@
+const plantNeedsWater = day => day === 'Wednesday' ? true : false;
+
+/* JavaScript provides ways to refactor arrow syntax. The most condensed form of the function is known as concise body.
