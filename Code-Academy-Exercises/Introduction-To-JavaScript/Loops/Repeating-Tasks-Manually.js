08a32b07ac8cc9a2b8eac690c04f5df76050918c
sheknowB <bailey.brandon.k@gmail.com>
2018-10-25 12:04:58 -1000

    Repeating code manually

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Repeating-Tasks-Manually.js
@@ -0,0 +1,7 @@
+// Write your code below
+const vacationSpots = ['Australia', 'Azores', 'Monaco'];//Create an array of places you would like to visit
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
+
+//Repeating code manually is cumbersome and time consuming, not to mention boring.
