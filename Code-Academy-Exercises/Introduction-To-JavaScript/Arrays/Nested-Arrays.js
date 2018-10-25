8ba6a2f07e1b8d12ce3570a0a278faea6a007b64
sheknowB <bailey.brandon.k@gmail.com>
2018-10-25 11:51:01 -1000

    Exploring nested arrays

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/Nested-Arrays.js
@@ -0,0 +1,4 @@
+const numberClusters = [[1, 2], [3, 4], [5, 6]];//Create a nested array named numberClusters
+const target = numberClusters[2][1];//Declare a variable named target and access element 6 inside of numberClusters
+
+//Nested arrays are arrays stored in another array.
