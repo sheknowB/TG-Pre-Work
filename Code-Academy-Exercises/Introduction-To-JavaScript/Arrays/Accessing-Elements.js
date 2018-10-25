b47aa11c0f42ce4c028529c8b0b88fb1ab40c423
sheknowB <bailey.brandon.k@gmail.com>
2018-10-25 10:24:40 -1000

    Indexing lets us choose individual elements.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/Accessing-Elements.js
@@ -0,0 +1,10 @@
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+const listItem = famousSayings[0];
+console.log(listItem);
+console.log(famousSayings[2]);
+console.log(famousSayings[3]);//Prints undefined
+
+/* As mentioned in the previous exercise, elements have a numbered position within an Array. These positions are known as indexes. We can access an element according to its index.
+  Arrays in JavaScript are zero-indexed, meaning the first element is index 0.
+  You can also access individual characters in a string using bracket notation and the index.
+*/
