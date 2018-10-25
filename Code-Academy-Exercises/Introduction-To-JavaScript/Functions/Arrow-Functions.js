f02bfc6fd624f936a29c8b983f7db14b98c108ec
sheknowB <bailey.brandon.k@gmail.com>
2018-10-24 21:06:34 -1000

    Arrow Functions. fat arrow () => notation.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Arrow-Functions.js
@@ -0,0 +1,10 @@
+const plantNeedsWater = (day) => {
+  if (day === 'Wednesday') {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+/* Arrow functions remove the need to type out the keyword function every time you need to create a function.
+  The arrow function notation is known as the fat arrow () => notation.
