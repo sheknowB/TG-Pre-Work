6b4a34692c6a8f5c918a3a15e815c0fc7243a26e
sheknowB <bailey.brandon.k@gmail.com>
2018-10-24 18:06:58 -1000

    Function Declaration exercise

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Function-Declaration.js
@@ -0,0 +1,13 @@
+function getReminder() {
+  console.log ('Water the plants.')
+}
+
+function greetInSpanish() {
+  console.log ('Buenas Tardes.')
+}
+/* One way to create a function is by using a function declaration. A function declaration binds a function to a name, or an identifier.
+  A function declaration consists of:
+    - The function keyword
+    - The name of the function, or its identifier, followed by a parantheses
+    - A function body, or the block of statements required to perform a specific task, enclosed in the functions curly brackets {}
+*/
