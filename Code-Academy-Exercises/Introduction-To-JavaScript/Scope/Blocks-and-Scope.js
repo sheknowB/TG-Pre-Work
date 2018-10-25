f18d8ab6e2f1bdb98d0a47bd159bae68749c1191
sheknowB <bailey.brandon.k@gmail.com>
2018-10-25 09:10:51 -1000

    Prints "The stars over the Empire State Building in New York City.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Scope/Blocks-and-Scope.js
@@ -0,0 +1,8 @@
+const city = "New York City";
+function logCitySkyline() {
+  let skyscraper = "Empire State Building";
+  return 'The stars over the ' + skyscraper + ' in ' + city;
+};
+console.log(logCitySkyline());
+
+//Blocks, found in curly brackes {}, help us group one or more statements together and serve as an important structural marker for our code.
