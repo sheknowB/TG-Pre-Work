f033cd9a5ea68c1e4b06f7f8140e5a155dfab4fd
sheknowB <bailey.brandon.k@gmail.com>
2018-10-24 18:50:36 -1000

    Exploring Parameters and Arguments

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Parameters-and-Arguments.js
@@ -0,0 +1,8 @@
+function sayThanks(name) {
+  console.log('Thank you for your purchase '+ name + '! We appreciate your business.');
+}
+sayThanks('Cole');
+/* Parameters allow functions to accept input(s) and perform a task using those input(s).
+  Parameters are used as placeholders for information that will be passed to the function when its called.
+  The values that are passed to the function when it is called are called arguments. Arguments can be passed on as values or variables.
+  The order in which arguments are passed and assigned follows the order that the parameters are declared.
