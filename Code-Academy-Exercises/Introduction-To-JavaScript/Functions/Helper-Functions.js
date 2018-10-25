4ad224e73c3e592f2986dfd8d60b8f8c4c214ee3
sheknowB <bailey.brandon.k@gmail.com>
2018-10-24 20:34:42 -1000

    Helper Functions.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Helper-Functions.js
@@ -0,0 +1,10 @@
+function monitorCount(rows, columns) {
+  return rows * columns;
+}
+function costOfMonitors(rows, columns) {
+  return monitorCount(rows, columns) * 200;
+}
+const totalCost = costOfMonitors(5, 4);
+console.log(totalCost);
+
+// When returning a value of a function inside another function. The function being called within another function is referred to as a helper function.
