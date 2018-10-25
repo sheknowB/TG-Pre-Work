da12d8fce8ac0d3744e4fea5ac797e41ae37ab85
sheknowB <bailey.brandon.k@gmail.com>
2018-10-25 11:07:50 -1000

    Implementing the .pop() method to the chores array.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/The-pop-method.js
@@ -0,0 +1,6 @@
+const chores = ['wash dishes', 'do laundry', 'take out trash', 'cook dinner', 'mop floor'];
+
+chores.pop();//Remove last item of chores array
+console.log(chores);
+
+//The .pop() method removes the last item of an array.
