4e531c724fb0d29905f9548c595fbfe639143463
sheknowB <bailey.brandon.k@gmail.com>
2018-10-25 09:28:11 -1000

    Exploring global and local variables.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Scope/Block-Scope.js
@@ -0,0 +1,12 @@
+const logVisibleLightWaves = () => {
+  const lightWaves = 'Moonlight';
+  console.log(lightWaves);
+}
+logVisibleLightWaves();
+//Prints Moonlight
+console.log(lightWaves);
+//ReferenceError
+
+/* When a variable is defined in a block, it is only accessible to the code within the cury braces {}.
+  We call these block scope variables because it is only accessible to the lines of code within that block. These are known as local variables.
+*/
