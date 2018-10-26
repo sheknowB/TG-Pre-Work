192ab2bcc2b57eb5a2fe69fb4b8f807b874c2c05
sheknowB <bailey.brandon.k@gmail.com>
2018-10-25 20:06:52 -1000

    Do...while statement that adds cups of sugar until it reaches the cups of sugar needed value.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Do-While-Statements.js
@@ -0,0 +1,13 @@
+// Write your code below
+cupsOfSugarNeeded = 9;
+let cupsAdded = 0;
+
+do {
+  cupsAdded++;
+} while (cupsAdded < cupsOfSugarNeeded);
+
+console.log(cupsAdded);
+
+/* A Do...while statement says to do a task once and then keep doing it until a specified condition is no longer met.
+  While statements are different from Do...while statements because a Do...while statement will run at least once whether or not the condition evaluates to true.
+*/
