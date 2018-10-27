c4af0ca34f69c079e2ae3cb965829b6352f19d34
sheknowB <bailey.brandon.k@gmail.com>
2018-10-27 13:44:15 -1000

    constructor() method

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Classes/Constructor.js
@@ -0,0 +1,8 @@
+class Surgeon {
+  constructor(name, department) {
+  	this.name = name;
+    this.department = department;
+  }
+}
+
+// Within Classes, the constructor method is called upon when a new instance of a class is created.
