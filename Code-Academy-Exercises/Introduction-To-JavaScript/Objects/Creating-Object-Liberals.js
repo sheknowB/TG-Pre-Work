3d7fbcb05f85a21fe9727f4dd2f06a8a3a4ccfa2
sheknowB <bailey.brandon.k@gmail.com>
2018-10-26 15:14:59 -1000

    fasterShip object literal

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Objects/Creating-Object-Liberals.js
@@ -0,0 +1,8 @@
+// Write your fasterShip object literal below
+let fasterShip = {
+  'Fuel Type': 'Turbo Fuel',
+  color: 'silver'
+};
+/* Objects can be assigned to variables. We use curly brackets {} to designate object literal. We fill an object with unordered data. This data is organized into key-value pairs. The key is like a variable name that points to a location in memory that holds a value.
+  To make a key-value pair we write the key's name (identifier), followed by a colon and then the value. We separate key-value pairs by a comma.
+*/
