97c73570660e461b9290283a95fc0ea3b8d811b9
sheknowB <bailey.brandon.k@gmail.com>
2018-10-27 15:45:08 -1000

    Inheritance, create a superclass that subclasses can share from

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Classes/Inheritance-1.js
@@ -0,0 +1,3 @@
+/* When you have multiple classes that share the same properties or methods, a developer can use inheritance which decreases the amount of code needed.
+  With inheritance a developer can create a parent class (superclass) with properties and methods that multiple child classes (subclass) can share.
+*/
