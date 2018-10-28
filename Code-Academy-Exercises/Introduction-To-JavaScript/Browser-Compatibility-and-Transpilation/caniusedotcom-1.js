e0d19dde615fcb1ad06652041b2dfa5ccb7bcea6
sheknowB <bailey.brandon.k@gmail.com>
2018-10-27 21:10:25 -1000

    A preview of how to use caniuse.com to look up browser support of JavaScript ES6 modules.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/caniusedotcom-1.js
@@ -0,0 +1,2 @@
+// caniuse.com is a good resource to look up what percentage of browsers use a new JavaScript syntax.
+// since companies add ES6 support gradually, and users may be slow to update their own browsers, it's good practice to search for browser support on a feature-by-feature basis.
