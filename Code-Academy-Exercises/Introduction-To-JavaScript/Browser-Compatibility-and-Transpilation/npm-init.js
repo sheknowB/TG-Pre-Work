83b8c8604ad8938b67116ed8190528d7e1e95b2f
sheknowB <bailey.brandon.k@gmail.com>
2018-10-27 21:44:50 -1000

    npm init command

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/npm-init.js
@@ -0,0 +1,8 @@
+/* npm init command creates a package.json file in the root directory of our project.
+  A package.json files contains information about the current JavaScript project.
+  Some of this information includes:
+
+    - Metadata: A project's name, title, description, authors, etc.
+    - A list of node packages required for the project
+    - Key-value pairs for command line script
+*/
