96609e21a3d2d70c13c27a04c7c700a85dbab89e
sheknowB <bailey.brandon.k@gmail.com>
2018-10-27 21:15:08 -1000

    using caniuse.com to search for JavaScript browser support

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/caniusedotcom-2.js
@@ -0,0 +1,7 @@
+// Set the variable below to a number
+let esFivePercentageSupport = 95.93;
+
+// Set the variable below to a number
+let esSixTemplateLiterals = 88.51;
+
+// Used the website caniuse.com to search for above percentages
