a612dd69b6b4ad8bc5a1438c402465e319f7e037
sheknowB <bailey.brandon.k@gmail.com>
2018-10-28 08:40:05 -1000

    Introduction to what are modules

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Intermediate-JavaScript-Modules/Hello-Modules.js
@@ -0,0 +1,8 @@
+/* JavaScript modules are reuseable pieces of code that can be exported from one program and imported for use in another program.
+
+   By seperating code into modules we can:
+    - find, fix, and debug code more easily
+    - reuse and recycle defined logic in different parts of our application
+    - keep information private and protected from other modules
+    - prevent pollution of the global namescape and potential naming collisions
+*/
