70396aa726d371a44da772887d03ad1653d7b99b
sheknowB <bailey.brandon.k@gmail.com>
2018-10-27 15:31:15 -1000

    Methods for Classes

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Classes/Methods.js
@@ -0,0 +1,28 @@
+class Surgeon {
+  constructor(name, department) {
+    this._name = name;
+    this._department = department;
+    this._remainingVacationDays = 20;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get department() {
+    return this._department;
+  }
+
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+}
+
+const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
+const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
+
+//The getter syntax is the same in Class as they are for Objects, except you can not include commas between methods
