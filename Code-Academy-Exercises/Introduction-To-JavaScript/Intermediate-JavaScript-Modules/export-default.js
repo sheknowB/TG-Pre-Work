660082d522c78325bad98d06a1ee1f7999c3c8dc
sheknowB <bailey.brandon.k@gmail.com>
2018-10-28 09:49:35 -1000

    Using export default

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Intermediate-JavaScript-Modules/export-default.js
@@ -0,0 +1,15 @@
+let Airplane = {};
+Airplane.availableAirplanes = [
+  {
+    name: 'AeroJet',
+    fuelCapacity: 800
+  },
+  {
+    name: 'SkyJet',
+    fuelCapacity: 500
+  }
+];
+
+export default Airplane;
+
+// ES6 has two new techniques to export data. One way is default import.
