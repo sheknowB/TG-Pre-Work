3cf50e7b5af18f1eb634194f92e77b0bc16d1e7f
sheknowB <bailey.brandon.k@gmail.com>
2018-10-27 21:04:46 -1000

    Introduction to browser compatibility issues

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/Introduction.js
@@ -0,0 +1,12 @@
+var pasta = "Spaghetti"; // ES5 syntax
+
+const meat = "Pancetta"; // ES6 syntax
+
+let sauce = "Eggs and cheese"; // ES6 syntax
+
+// Template literals, like the one below, were introduced in ES6
+const carbonara = `You can make carbonara with ${pasta}, ${meat}, and a sauce made with ${sauce}.`;
+
+/* Two tools to addressing web browser compatibility issues:
+  1.  caniuse.com: provides data on web browser compatibility for HTML, CSS, and JavaScript features.
+  2.  babel: a JavaScript library that can be used to convert new, unsupported JavaScriptm into an older version that can be recognized by browsers.
