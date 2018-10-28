28588c9f414c54f544bb79d3cc05a964c7236830
sheknowB <bailey.brandon.k@gmail.com>
2018-10-27 21:26:42 -1000

    Why ES6

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/Why-ES6.js
@@ -0,0 +1,15 @@
+var pasta = "Spaghetti"; // ES5 syntax
+
+var meat = "Pancetta"; // ES6 syntax
+
+var sauce = "Eggs and cheese"; // ES6 syntax
+
+// Template literals, like the one below, were introduced in ES6
+var carbonara = 'You can make carbonara with '+ pasta +', ' + meat +', and a sauce made with ' + sauce +'.';
+
+/* Three reasons for the ES5 -> ES6 update
+  1.  JavaScript ES6 is syntactically more similar to other object-oriented programming languages, making it easier for non-JavaScript programmers to learn JavaScript.
+  2.  Readability and economy of code. easier to understand, and can do more in less characters.
+  3.  Addresses sources of ES5 bugs.
+
+  To limit the impact of ES6 browser compatibility issues, Ecma made the new syntax backwards compatible.
