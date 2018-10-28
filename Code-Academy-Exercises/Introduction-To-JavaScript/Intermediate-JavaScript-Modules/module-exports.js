a306ced28d17c65ddf0e7346c24dc728e96145f8
sheknowB <bailey.brandon.k@gmail.com>
2018-10-28 08:54:10 -1000

    Exporting module Airplane

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Intermediate-JavaScript-Modules/module-exports.js
@@ -0,0 +1,12 @@
+let Airplane = {}; // Create an object named Airplane
+Airplane.myAirplane = "StarJet"; // Add property myAirplane
+
+module.exports = Airplane; // Export the module Airplane
+
+/* When working with modules, we define them and then export them using the statement module.exports. module is a variable that represents the module, and exports exposes the module as an object.
+
+  The pattern of exporting a module:
+    1.  Define an object to represent the module
+    2.  Add data or behavior to the module
+    3.  Export the module
+*/
