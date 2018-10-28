3dae428668dcb315a344d6ff0683e24ee11c19b9
sheknowB <bailey.brandon.k@gmail.com>
2018-10-27 22:19:04 -1000

    running the npm run build to transpile ES6+ JavaScript into ES5.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/Build.js
@@ -0,0 +1,8 @@
+/* After writing the "build" script into the package.json we can call "build" from the command line to transpile and write ES5 code to a directory called lib.
+
+   From the command line we write: npm run build
+   This runs the build script in package.json. Babel writes the ES5 code to a file named main.js inside of a folder called lib.
+
+  The npm run build command will transpile all JavaScript files inside of the src folder.
+  You will only need to run the npm run build command once to transpile all of your code.
+*/  
