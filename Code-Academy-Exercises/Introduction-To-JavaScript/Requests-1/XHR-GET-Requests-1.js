b5a070d2fab42422ab1f8dd50ec2caaebf854241
sheknowB <bailey.brandon.k@gmail.com>
2018-10-29 10:47:57 -1000

    XHR Get Requests Introduction

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Requests-1/XHR-GET-Requests-1.js
@@ -0,0 +1,3 @@
+/*  Asynchronous JavaScript and XML (AJAX), enabales requests to be made after the initial page load.
+    Similarly, the XMLHttpRequest (XHR) API, named for XML, can be used to make many kinds of requests and supports other forms of data.
+*/
