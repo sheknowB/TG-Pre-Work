8f8797b4286ade0863abb4f3af8c009dc98586c8
sheknowB <bailey.brandon.k@gmail.com>
2018-10-29 10:31:40 -1000

    Introduction to HTTP requests

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Requests-1/Introduction-to-Requests.js
@@ -0,0 +1,4 @@
+/*  the four most commonly used HTTP Requests are GET, POST, PUT and DELETE.
+  With a GET request we are retrieving information from some source.
+  For a POST request we're posting information to a source that will process the information and send it back.
+*/   
