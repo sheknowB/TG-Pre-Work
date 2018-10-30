99221402a3250a9c184136bd07f3494bfe84ed7c
sheknowB <bailey.brandon.k@gmail.com>
2018-10-30 09:15:24 -1000

    Manipulated previous code to access the Datamuse API

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Requests-2/fetch-GET-Requests-3.js
@@ -0,0 +1,35 @@
+// Information to reach API
+const url = 'https://api.datamuse.com/words';
+const queryParams = '?sl=';
+
+// Selects page elements
+const inputField = document.querySelector('#input');
+const submit = document.querySelector('#submit');
+const responseField = document.querySelector('#responseField');
+
+// AJAX function
+const getSuggestions = () => {
+  const wordQuery = inputField.value;
+  const endpoint = `${url}${queryParams}${wordQuery}`;
+  fetch(endpoint).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('Request failed!');
+  }, networkError => {
+    console.log(networkError.message);
+  });
+}
+
+// Clears previous results and display results to webpage
+const displaySuggestions = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  }
+  getSuggestions();
+};
+
+submit.addEventListener('click', displaySuggestions);
+
+//  In this exercise we used the boilerplate code we made in the previous exercise and manipulated it to access the Datamuse API.
